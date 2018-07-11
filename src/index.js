f6598fdbfa140d408145a6032cf826660ee0ce1c
adamewhite <evilofbanality@gmail.com>
2018-07-10 21:00:11 -0400

    more news

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
 const Main = () => (
-  <BrowserRouter basename={'/ltrain'}>
+  <BrowserRouter>
     <Route path="/" component={App} />
   </BrowserRouter>
 );
